6c68e10afc1b7e9fe01774a092c30ef2d2332f12
Mack Kemper <mackkemper@gmail.com>
2019-11-20 21:46:23 -0700

    Week 12 - Fn 3
    Week 12 - Function 3 Complete

--- Week12-utility.py
+++ Week12-utility.py
@@ -11,3 +11,10 @@ def LoadFile(loc):
             line = line.rstrip()
             line_list.append(line)
     return(line_list)
+
+# UpdateString
+def UpdateString(string, char, index):
+    string_list = list(string)
+    string_list[index] = char
+    new_string = "".join(string_list)
+    print("OUTPUT ", new_string)
