0fc9bd7f5af85ae017c8473af2b03d3e6e925f40
Mack Kemper <mackkemper@gmail.com>
2019-11-20 21:40:29 -0700

    Week 12 - Fn 2
    Week 12 - Function 2 Complete

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,13 @@
 # PrintOutput
 def PrintOutput(thing):
     print("OUTPUT ", thing)
+
+# LoadFile
+def LoadFile(loc):
+    line_list = []
+    with open(loc) as doc:
+        lines = doc.readlines()
+        for line in lines:
+            line = line.rstrip()
+            line_list.append(line)
+    return(line_list)
